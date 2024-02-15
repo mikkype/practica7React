0c9af16976827c33faf50282c11bad10878f6b06
miguel <miguelcaceda@gmail.com>
2024-02-15 13:03:48 +0100

    eje2 a b

--- book-app/src/components/library.js
+++ book-app/src/components/library.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { useState,useEffect } from "react";
 
 function LibraryComponent(){
+    const [library, setLibrarys] = useState([])
 
-    return(
-        <>
-        <h2>Books List</h2>
-        </>
-    )
+    useEffect(() =>{
+        fetch('http://localhost:3030/library')
+        .then ((response)=>{
+            return response.json()
+        })
+        .then((library)=> {
+            setLibrarys(library)
+        })
+    },[])
 }
 
 export default LibraryComponent;
